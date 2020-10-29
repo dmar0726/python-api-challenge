5218c48c1b0c2857290d41289d6052d69afebee2
David Marobella <david.marobella@bcbsnc.com>
2020-10-28 21:06:18 -0400

    API HW

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9bc2a5a7b0488787cadfb0406c529977"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDgNjJ1CLnOlyP-hIfWwbw3UCuetZKx21I"
